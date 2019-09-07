0b9eb7a1ddb74a1d32bba50ec90a5405b7b8934d
Valentin Raduti <valentin@thedotin.ro>
2019-09-07 21:09:16 +0300

    all features demo

--- VictoryDemo.js
+++ VictoryDemo.js
@@ -1,15 +1,72 @@
 import React from "react";
-import { StyleSheet, View, Picker, Button } from "react-native";
-import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis, VictorySelectionContainer } from "victory-native";
+import { StyleSheet, View, Picker, Button, ImageBackground, Animated, Image, PanResponder } from "react-native";
+import { VictoryLine, VictoryTooltip, VictoryVoronoiContainer, VictoryLabel, VictoryBar, VictoryArea, VictoryChart, VictoryTheme, VictoryAxis, VictoryCursorContainer, createContainer, VictoryZoomContainer } from "victory-native";
+import { Svg } from "react-native-svg"
+import { PinchGestureHandler, PanGestureHandler } from "react-native-gesture-handler"
+import { theme } from "./VictoryTheme"
+
+const ranges = {
+  '3y': { start: new Date(2017, 1, 1).getTime(), end: new Date().getTime() },
+  'ytd': { start: new Date(2019, 1, 1).getTime(), end: new Date().getTime() },
+  '6m': { start: new Date(2019, 3, 1).getTime(), end: new Date().getTime() },
+  '1m': { start: new Date(2019, 8, 1).getTime(), end: new Date().getTime() }
+};
 
 export default class VictoryDemo extends React.Component {
   constructor() {
     super();
-    this.state = { count: 50, data: [] };
+    this.state = { count: 50, range: '3y', data: [] };
   }
 
   componentDidMount() {
-    this.changeData(50);
+    this.changeData(50, ranges[this.state.range].start, ranges[this.state.range].end);
+    this._panResponder = PanResponder.create({
+      onPanResponderGrant: () => { },
+      onPanResponderTerminate: () => { },
+      onMoveShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponder: () => true,
+      onShouldBlockNativeResponder: () => true,
+      onPanResponderTerminationRequest: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onStartShouldSetPanResponderCapture: () => true,
+      onPanResponderMove: (evt) => {
+        const touches = evt.nativeEvent.touches;
+        if (touches.length === 2) {
+          if (!this.state.isZooming) {
+            this.setState({ isZooming: true, zoomStart: Math.abs(touches[0].locationX - touches[1].locationX) })
+          }
+          this.setState({ zoomEnd: Math.abs(touches[0].locationX - touches[1].locationX) })
+        }
+        if (!this.state.isPanning) {
+          this.setState({ isPanning: true, panStart: touches[0].locationX })
+        }
+        if (touches.length === 1) {
+          this.setState({ panEnd: touches[0].locationX })
+        }
+      },
+      onPanResponderRelease: (evt) => {
+        if (this.state.isZooming) {
+          if (this.state.zoomStart < this.state.zoomEnd) {
+            //Zoom out
+            this.selectNextRange();
+          } else {
+            this.selectPrevRange();
+          }
+        }
+        if (this.state.isPanning) {
+          if (this.state.panStart < this.state.panEnd) {
+            // Pan right
+            this.selectNextRange();
+          } else {
+            this.selectPrevRange();
+          }
+        }
+        this.setState({
+          isZooming: false,
+          isPanning: false,
+        });
+      },
+    });
   }
 
   static navigationOptions = {
@@ -22,7 +79,7 @@ export default class VictoryDemo extends React.Component {
     for (let d = start; d < end; d += interval) {
       data.push({ a: new Date(d), b: Math.random() * 2000 + 1000 });
     }
-    this.setState({ count: count, data: data });
+    this.setState({ count, data });
   }
 
   selectZoom(points, bounds, props) {
@@ -31,45 +88,62 @@ export default class VictoryDemo extends React.Component {
     return false;
   }
 
+  selectRange(range) {
+    this.setState({ range }, () =>
+      this.changeData(this.state.count, ranges[range].start, ranges[range].end));
+    return false;
+  }
+
+  selectNextRange() {
+    const i = Object.keys(ranges).indexOf(this.state.range);
+    if (i >= 0 && i + 1 < Object.keys(ranges).length) {
+      this.selectRange(Object.keys(ranges)[i + 1])
+    }
+    return []
+  }
+
+  selectPrevRange() {
+    const i = Object.keys(ranges).indexOf(this.state.range);
+    if (i - 1 >= 0)
+      this.selectRange(Object.keys(ranges)[i - 1])
+  }
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Picker
-          selectedValue={this.state.count || 50}
-          style={{ height: 50, width: 100 }}
-          onValueChange={(itemValue, itemIndex) =>
-            this.changeData(itemValue)
-          }>
-          <Picker.Item label='50' value={50} />
-          <Picker.Item label='500' value={5000} />
-          <Picker.Item label='5000' value={50000} />
-        </Picker>
-        <Button
-          title="Reset"
-          onPress={() => this.changeData(50)}
-        />
-        {this.state.data.length > 0 && <VictoryChart width={350} height={300} theme={VictoryTheme.material} scale={{ x: "time" }}
-          containerComponent={
-            <VictorySelectionContainer
-              selectionDimension="x"
-              onSelection={this.selectZoom.bind(this)}
-            // onSelection={(a,b,c)=>console.error('bla', JSON.stringify(b))}
-            // selectionStyle={{
-            //   fill: "tomato", fillOpacity: 0.5,
-            //   stroke: "tomato", strokeWidth: 2
-            // }}
-            />
-          }
-        >
-          <VictoryLine
-            style={{ data: { stroke: ({ active }) => active ? "tomato" : "gray" } }}
-            data={this.state.data}
-            x="a"
-            y="b"
+        <ImageBackground source={{ uri: 'https://ichef.bbci.co.uk/images/ic/1280xn/p070kck4.jpg' }}
+          style={styles.container}>
+          <Picker
+            selectedValue={this.state.count || 50}
+            style={{ height: 50, width: 100 }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.changeData(itemValue)
+            }>
+            <Picker.Item label='50' value={50} />
+            <Picker.Item label='500' value={5000} />
+            <Picker.Item label='5000' value={50000} />
+          </Picker>
+          <Button
+            title="Reset"
+            onPress={() => this.changeData(50)}
           />
+          {this.state.data.length > 0 &&
+            <View {...this._panResponder.panHandlers}>
+              <VictoryChart width={320} height={300} theme={theme} scale={{ x: "time" }}>
+                <VictoryArea
+                  labelComponent={<VictoryTooltip />}
+                  labels={({ datum }) => datum.y}
+                  data={this.state.data}
+                  x="a"
+                  y="b"
 
-        </VictoryChart>
-        }
+                />
+
+              </VictoryChart>
+            </View>
+          }
+        </ImageBackground>
       </View>
     );
   }
@@ -79,7 +153,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff"
+    alignItems: "center"
+  },
+  semi: {
+    backgroundColor: "rgba(256,256,256,.3)"
   }
 });
--- WrapperDemo.js
+++ WrapperDemo.js
@@ -3,13 +3,21 @@ import {
   Picker,
   StyleSheet,
   Text,
-  View, processColor
+  ImageBackground,
+  View, processColor,
+  PanResponder
 } from 'react-native';
 
 //https://github.com/wuxudong/react-native-charts-wrapper/tree/master/Example/app
 
 import { LineChart } from 'react-native-charts-wrapper';
 
+const ranges = {
+  '3y': { start: new Date(2017, 1, 1).getTime(), end: new Date().getTime() },
+  'ytd': { start: new Date(2019, 1, 1).getTime(), end: new Date().getTime() },
+  '6m': { start: new Date(2019, 3, 1).getTime(), end: new Date().getTime() },
+  '1m': { start: new Date(2019, 8, 1).getTime(), end: new Date().getTime() }
+};
 
 export default class WrapperDemo extends React.Component {
   static navigationOptions = {
@@ -17,48 +25,170 @@ export default class WrapperDemo extends React.Component {
   };
   constructor() {
     super();
-    this.state = { count: 50, data: [] };
+    this.state = { count: 50, range: '3y', data: [] };
   }
   componentDidMount() {
-    this.changeData(50);
+    this.changeData(50, ranges[this.state.range].start, ranges[this.state.range].end);
+    this._panResponder = PanResponder.create({
+      onPanResponderGrant: () => { },
+      onPanResponderTerminate: () => { },
+      onMoveShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponder: () => true,
+      onShouldBlockNativeResponder: () => true,
+      onPanResponderTerminationRequest: () => true,
+      onMoveShouldSetPanResponderCapture: () => true,
+      onStartShouldSetPanResponderCapture: () => true,
+      onPanResponderMove: (evt) => {
+        const touches = evt.nativeEvent.touches;
+        if (touches.length === 2) {
+          if (!this.state.isZooming) {
+            this.setState({ isZooming: true, zoomStart: Math.abs(touches[0].locationX - touches[1].locationX) })
+          }
+          this.setState({ zoomEnd: Math.abs(touches[0].locationX - touches[1].locationX) })
+        }
+        if (!this.state.isPanning) {
+          this.setState({ isPanning: true, panStart: touches[0].locationX })
+        }
+        if (touches.length === 1) {
+          this.setState({ panEnd: touches[0].locationX })
+        }
+      },
+      onPanResponderRelease: (evt) => {
+        if (this.state.isZooming) {
+          if (this.state.zoomStart < this.state.zoomEnd) {
+            //Zoom out
+            this.selectNextRange();
+          } else {
+            this.selectPrevRange();
+          }
+        }
+        if (this.state.isPanning) {
+          if (this.state.panStart < this.state.panEnd) {
+            // Pan right
+            this.selectNextRange();
+          } else {
+            this.selectPrevRange();
+          }
+        }
+        this.setState({
+          isZooming: false,
+          isPanning: false,
+        });
+      },
+    });
   }
 
-  changeData(count) {
+
+  changeData(count, start = new Date(2016, 1, 1).getTime(), end = new Date().getTime()) {
     const data = [];
-    const start = new Date(2016, 1, 1).getTime();
-    const end = new Date().getTime();
     const interval = (end - start) / count;
     for (let d = start; d < end; d += interval) {
       data.push({ x: new Date(d).getTime(), y: Math.random() * 2000 + 1000 });
     }
-    this.setState({ count: count, data: data });
+    this.setState({ count, data });
+  }
+
+  selectZoom(points, bounds, props) {
+    // console.error(points,bounds,props);
+    this.changeData(this.state.count, new Date(bounds.x[0]).getTime(), new Date(bounds.x[1]).getTime());
+    return false;
+  }
+
+  selectRange(range) {
+    this.setState({ range }, () =>
+      this.changeData(this.state.count, ranges[range].start, ranges[range].end));
+    return false;
+  }
+
+  selectNextRange() {
+    const i = Object.keys(ranges).indexOf(this.state.range);
+    if (i >= 0 && i + 1 < Object.keys(ranges).length) {
+      this.selectRange(Object.keys(ranges)[i + 1])
+    }
+    return []
+  }
+
+  selectPrevRange() {
+    const i = Object.keys(ranges).indexOf(this.state.range);
+    if (i - 1 >= 0)
+      this.selectRange(Object.keys(ranges)[i - 1])
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Picker
-          selectedValue={this.state.count || 50}
-          style={{ height: 50, width: 100 }}
-          onValueChange={(itemValue, itemIndex) =>
-            this.changeData(itemValue)
-          }>
-          <Picker.Item label='50' value={50} />
-          <Picker.Item label='500' value={5000} />
-          <Picker.Item label='5000' value={50000} />
-        </Picker>
-        <LineChart style={styles.chart}
-          legend={{ enabled: false }}
-          xAxis={{
-            valueFormatter: 'date',
-            valueFormatterPattern: 'YYYY-M',
-            position: 'BOTTOM',
-            granularityEnabled: true,
-            granularity: 1,
-            labelCount: 5,
-          }}
-          data={{ dataSets: [{ label: "demo", values: this.state.data }] }}
-        />
+        <ImageBackground source={{ uri: 'https://ichef.bbci.co.uk/images/ic/1280xn/p070kck4.jpg' }}
+          style={styles.container}>
+          <Picker
+            selectedValue={this.state.count || 50}
+            style={{ height: 50, width: 100 }}
+            onValueChange={(itemValue, itemIndex) =>
+              this.changeData(itemValue)
+            }>
+            <Picker.Item label='50' value={50} />
+            <Picker.Item label='500' value={5000} />
+            <Picker.Item label='5000' value={50000} />
+          </Picker>
+          {this.state.data.length > 0 && <View {...this._panResponder.panHandlers} style={styles.container}>
+            <LineChart style={styles.chart}
+              legend={{ enabled: false }}
+              xAxis={{
+                valueFormatter: 'date',
+                valueFormatterPattern: 'YYYY-M',
+                position: 'BOTTOM',
+                granularityEnabled: true,
+                granularity: 1,
+                labelCount: 5,
+                drawGridLines: false,
+                gridColor: processColor('white'),
+                textColor: processColor('white'),
+              }}
+              yAxis={{
+                left: {
+                  drawGridLines: false,
+                  gridColor: processColor('white'),
+                  textColor: processColor('white'),
+                },
+                right: { enabled: false }
+              }}
+              data={{
+                dataSets: [{
+                  label: "demo", values: this.state.data,
+                  config: {
+                    drawCircles: true,
+                    drawCircleHole: true,
+                    circleColor: processColor('white'),
+                    drawValues: false,
+                    drawFilled: true,
+                    fillColor: processColor('#fafaf3'),
+                    fillAlpha: 700,
+                    color: processColor('white'),
+                    barShadowColor: processColor('lightgrey'),
+                    drawHighlightIndicators: true,
+                    drawHorizontalHighlightIndicator: false,
+                    highlightLineWidth: 10,
+                    highlightColor: processColor('gray')
+                  }
+                }]
+              }}
+              chartDescription={{ text: '' }}
+              dragEnabled={false}
+              scaleEnabled={false}
+              scaleXEnabled={false}
+              scaleYEnabled={false}
+              pinchZoom={false}
+              doubleTapToZoomEnabled={false}
+              marker={{
+                enabled: true,
+                digits: 2,
+                backgroundTint: processColor('gray'),
+                markerColor: processColor('navy'),
+                textColor: processColor('white'),
+                textSize: 14
+              }}
+            />
+          </View>}
+        </ImageBackground>
       </View>
     );
   }
@@ -66,13 +196,9 @@ export default class WrapperDemo extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#f5fcff"
+    flex: 1
   },
   chart: {
-    width: 350,
-    height: 300
+    height: 250
   }
 });
